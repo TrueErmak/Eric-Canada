4f664d146edb1979095f1fc8d6144ffc04c2a88f
TrueErmak <ecanada07@gmail.com>
2023-11-08 18:13:31 -0600

    start

--- src/App.js
+++ src/App.js
@@ -8,97 +8,106 @@ function Home() {
   return (
     <div className="container">
       <div className="page">
-        <h1>Home Page</h1>
+        <h1>about me</h1>
         {
-          <p className="brief-header"> Below is the presentation regarding the menemo project  </p>
+          <p className="brief-header"> Welcome to my personal page! My name is Eric Canada, and I am a passionate technologist currently advancing my expertise in Artificial Intelligence at the University of Advancing Technology (UAT). My academic journey is a blend of rigorous theory and hands-on experience, preparing me to contribute to the ever-evolving landscape of AI. Professionally, I stand at the intersection of technology and craftsmanship as a lathe programmer specializing in the Aerospace industry. My role involves precision and attention to detail, ensuring that the components we produce meet the exacting standards this field demands. Outside of my professional life, I am an avid technophile with a penchant for 3D printing. My home is my workshop, where I bring ideas to life using tools like Fusion 360 and other creative engines. There's a unique kind of joy in watching a concept materialize layer by layer, and it's this magic that fuels my enthusiasm for digital fabrication. But my skills are not confined to the virtual realm. As an experienced welder, I understand the value of traditional craftsmanship and its irreplaceable role in a world that's quickly moving towards the digital. Welding has taught me patience, precision, and the importance of a steady hand—skills that are surprisingly synergistic with programming and AI development. Speaking of programming, it is another arena where I thrive. Building programs and crafting Python applications is more than a job or a hobby; it's a means to problem-solve, to innovate, and to create tools that could one day change how we interact with technology. Whether it's through lines of code, a welded joint, or a 3D printed model, I find my calling in creation. It is in the act of making new things, improving existing ones, and learning from the process that I find my professional fulfillment. If you're curious about my work or share similar interests, feel free to connect. I am always open to discussing new technologies, potential collaborations, or simply exchanging ideas with fellow enthusiasts.  </p>
         }
       </div>
       {<video width="50%" height="auto" controls>
-          <source src="/sip-vpresintation.mkv" type="video/mp4" />
+          <source src="/" type="video/mp4" />
           Your browser does not support the video tag.
         </video>}
+
+        <img
+        src="/tiger-shirt.jpg" // Replace with the actual path to your image file
+        alt="eric canada" // Provide a brief description of the image for accessibility
+        width="335" // Set the width of the image (adjust as needed)
+        height="335" // Set the height of the image (adjust as needed)
+/>
+
 
         
     </div>
   );
 }
 
-function InnovativeBrief() {
+function EmploymentInformation() {
   return (
     <div className="container">
       <div className="page">
-        <h1>the Inovative Brief</h1>
+        <h1>employment Information</h1>
         {
           
-          <p className="brief-header"> Technical Field</p>
+          <p className="brief-header"> Eric Canada</p>
         }
         {
           
-          <p className="brief-common-text"> Web Development, Machine Learning & computer Vision, Digital Media Processing, Cloud & Environment Management, User Interface (UI) & user Experience (UX) Design, data Management & Storage</p>
+          <p className="brief-common-text"> [*** south 10th street Oregon IL] [815-238-5822] [ecanada07@gmail.com] </p>
         }
 
 
 {
           
-          <p className="brief-header"> Background Information</p>
+          <p className="brief-header"> Objectives:</p>
         }
         {
           
-          <p className="brief-common-text"> ChatGPT and the pursuit general artificial intelligence are the heart of the inspiration behind this project. one of the reasons I am pursuing this project because of the experience and new understanding that comes with crating a project like this, I hope to integrate this in my home systems. </p>
+          <p className="brief-common-text"> Highly skilled artificial intelligence graduate with hands-on experience in aerospace machining and 3D printing business operations. Seeking a position in multiple production based positions that leverages my technical acumen and entrepreneurial spirit.</p>
         }
 
 
 {
           
-          <p className="brief-header"> Prior Art legal term</p>
+          <p className="brief-header"> Education:</p>
         }
         {
           
-          <p className="brief-common-text"> Prior art is a legal term from patents. It should describe any sources of inspiration for your project, any sources currently on the market that might be considered competitors, and any similar work that your SIP might build upon. By the end of this section, your reader should have a clear view of the existing marketplace. Do not just list sources, but analyze their pros and cons.</p>
+          <p className="brief-common-text">University of Advancing Technology (UAT)
+          Bachelor of Science in Artificial Intelligence & Robotics/ Embedded systems.  
+          [from 6,15,2020 to ] 
+          </p>
         }
 
 
 
 {
           
-          <p className="brief-header"> Project Description</p>
+          <p className="brief-header"> Job History: </p>
         }
+
         {
-          
-          <p className="brief-common-text"> This project currently uses artificial intelligence to process video and text information. The project also has an interactive element that allows the user to asker the A.I questions about the information the user provides. </p>
+          <p className="brief-common-text">(1) PMI Aerospace: [9/7/2021] [current] Tittle: [Machinist & Operations Specialist] @ https://pmiaerospace.com/ </p>
+
         }
+        
+        <Link to="/PMIAerospace" className="link-style">Click to Investigate Job Discription</Link> 
+        
 
-
-
-
-{
           
-          <p className="brief-header"> Innovation Claim</p>
-        }
+        
         {
-          
-          <p className="brief-common-text"> MinimoView PWA uniquely combines advanced machine learning-driven video analysis with a seamless user interface in a web-based platform, bridging the gap between sophisticated AI capabilities and intuitive user engagement, setting a new benchmark in digital media processing applications.</p>
+          <p className="brief-common-text">(2) MacLean-Fogg Company: [start] [end] Tittle:[Machinist & Operations Specialist] @ https://macleanfogg.com/  </p>
         }
+        <Link to="/MacLeanFoggCompany" className="link-style">Click to Investigate Job Discription</Link>
+        
 
-{
-          
-          <p className="brief-header"> Usage Scenario</p>
-        }
         {
-          
-          <p className="brief-common-text"> Consider a situation where "DataMasters Inc." frequently works with varied data sources, like user reviews, survey responses, images, and occasional video clips. They've been looking for a tool that can process this data without the need to store it, ensuring the privacy of their information and the efficiency of the process.</p>
+          <p className="brief-common-text">(3)  SPG international SPG international [5,1,2016 to 6,1,16 and 10,1,2018 to 6,1,2019] https://www.spgusa.com/ </p>
         }
+        <Link to="/SPGinternational" className="link-style">Click to Investigate Job Discription</Link>
 
-{
-          
-          <p className="brief-header"> Description of Design Prototype </p>
-        }
         {
-          
-          <p className="brief-common-text">The MinimoView Progressive Web Application (PWA) is built on the React JavaScript library, which ensures compatibility across multiple devices and offers offline capabilities. The backend utilizes Python, specifically libraries such as OpenCV for video processing and PyTorch for AI-driven video analysis. The system leverages cloud storage and processing for scalable video uploads and analysis.
-          The prototype serves as a foundational framework for the final MinimoView PWA. While the prototype showcases core functionalities, the final project aims to incorporate additional features based on user feedback, improved AI algorithms for more accurate video analysis, and integrations with other platforms and tools for enhanced versatility. In essence, the design prototype is the stepping stone, allowing us to test, iterate, and perfect our approach for the final project implementation.
-           </p>
+          <p className="brief-common-text">(3) Universal tank and fabrication [8,1,2014 to 6,1,2015]  </p>
         }
+        <Link to="/Universaltankandfabrication" className="link-style">Click to Investigate Job Discription</Link>
+        
+
+
+
+
+
+
+
 
 
       </div>
@@ -106,30 +115,44 @@ function InnovativeBrief() {
   );
 }
 
-function Resources() {
-  return <h1>Resources Page</h1>;
-}
-
-function AboutUs() {
+function Skills() {
   return (
     <div className="container">
       <div className="page">
-        <h1>about us</h1>
-        {
-          <p>Welcome to my personal page! My name is Eric Canada, and I am a passionate technologist currently advancing my expertise in Artificial Intelligence at the University of Advancing Technology (UAT). My academic journey is a blend of rigorous theory and hands-on experience, preparing me to contribute to the ever-evolving landscape of AI.
+        <h1>personal skills</h1>
+        <p className="brief-header"> CNC Lathe Programming and Operation</p>
+        <p className="brief-common-text">Proficient in CNC lathe programming and operation within the aerospace industry. Skilled in setting up and running CNC lathes to precision specifications. </p>
 
-          Professionally, I stand at the intersection of technology and craftsmanship as a lathe programmer specializing in the Aerospace industry. My role involves precision and attention to detail, ensuring that the components we produce meet the exacting standards this field demands.
+        <p className="brief-header"> Welding Expertise </p>
+        <p className="brief-common-text"> Extensive experience in welding various types of metals, adhering to welding specifications, and ensuring high-quality welds. Strong understanding of welding techniques and safety protocols. </p>
+
+        <p className="brief-header"> Programming Languages </p>
+        <p className="brief-common-text"> Proficient in multiple programming languages </p>
+
+        <p className="brief-header"> Artificial Intelligence (AI) </p>
+        <p className="brief-common-text">  Pursuing skills in artificial intelligence, demonstrating a commitment to staying current with cutting-edge technologies and trends in AI development. </p>
+
+        <p className="brief-header"> 3D Printing </p>
+        <p className="brief-common-text"> Competent in operating various 3D printing machines, with the ability to create complex three-dimensional objects accurately. </p>
+
+        <p className="brief-header"> Server Administration </p>
+        <p className="brief-common-text"> Proficient in setting up and maintaining servers, ensuring their security, and optimizing performance for various applications. </p>
+
+        <p className="brief-header"> Version Control (Git) </p>
+        <p className="brief-common-text"> Skilled in using Git for version control, facilitating collaborative development and efficient tracking of code changes. </p>
+        
+      </div>
+    </div>
+  ); 
+}
+
+function ContactMe() {
+  return (
+    <div className="container">
+      <div className="page">
+        <h1>contact me page</h1>
+        {
           
-          Outside of my professional life, I am an avid technophile with a penchant for 3D printing. My home is my workshop, where I bring ideas to life using tools like Fusion 360 and other creative engines. There's a unique kind of joy in watching a concept materialize layer by layer, and it's this magic that fuels my enthusiasm for digital fabrication.
-          
-          But my skills are not confined to the virtual realm. As an experienced welder, I understand the value of traditional craftsmanship and its irreplaceable role in a world that's quickly moving towards the digital. Welding has taught me patience, precision, and the importance of a steady hand—skills that are surprisingly synergistic with programming and AI development.
-          
-          Speaking of programming, it is another arena where I thrive. Building programs and crafting Python applications is more than a job or a hobby; it's a means to problem-solve, to innovate, and to create tools that could one day change how we interact with technology.
-          
-          Whether it's through lines of code, a welded joint, or a 3D printed model, I find my calling in creation. It is in the act of making new things, improving existing ones, and learning from the process that I find my professional fulfillment.
-          
-          If you're curious about my work or share similar interests, feel free to connect. I am always open to discussing new technologies, potential collaborations, 
-          or simply exchanging ideas with fellow enthusiasts.</p>
         }
       </div>
     </div>
@@ -138,15 +161,68 @@ function AboutUs() {
 }
 
 
+function PMIAerospace() {
+  return (
+    <div>
+      {/* Add content for the JobDescription page */}
+      <h2>Job Description</h2>
+      <p className="brief-common-text"> (1)	Operate and maintain a diverse range of machines, including 3 FEMCO HL-25s, 1 Takisawa TS-4000YS, and 1 Miyano LL-21 twin turret dual spindle lathe.</p>
+      <p className="brief-common-text"> (2)	Specialize in manual programming of all machines to ensure accuracy and optimal performance. </p>
+      <p className="brief-common-text"> (3)	Expertise in tool installation across various machines, ensuring efficient workflow.</p>
+      <p className="brief-common-text"> (4)	Spearhead job development, refining details in real-time as each job progresses. </p>
+      <p className="brief-common-text"> (5)	Proficient in reading and drafting technical prints for aerospace parts. </p>
+      <p className="brief-common-text">(6)	Design 2-dimensional plots for machine geometry as required. </p>
+      <p className="brief-common-text">(7)	Systematically organize equipment and tooling supplies, enhancing shop efficiency and accessibility. </p>
+      <p className="brief-common-text"> (8)	Oversee part construction executed by adjacent machinists, ensuring quality and adherence to specifications.</p>
+      <p className="brief-common-text">(9)	Procure specialized tooling as required for advanced alloys like Satellite 6B and carbide materials. </p>
+      <p className="brief-common-text">(10)	Conduct meticulous inspection of fabricated parts using advanced tools such as a 3D vision measuring system (with or without touch probes) and comparators. </p>
+      <p className="brief-common-text">(11)	Actively track part processes and implement corrective actions as necessary, ensuring high-quality fabrication outcomes. </p>
+      <p className="brief-common-text">(12)	Efficiently schedule jobs to machines, optimizing shop productivity. </p>
+      <p className="brief-common-text"> </p>
+      
+      
+    </div>
+  );
+}
 
-// Define the HomeComponent
+function MacLeanFoggCompany() {
+  return (
+    <div>
+      {/* Add content for the JobDescription page */}
+      <h2>MacLeanFoggCompany</h2>
+      <p>This is the job description page.</p>
+    </div>
+  );
+}
+
+function SPGinternational() {
+  return (
+    <div>
+      {/* Add content for the JobDescription page */}
+      <h2>SPGinternational</h2>
+      <p>This is the job description page.</p>
+    </div>
+  );
+}
+
+function Universaltankandfabrication() {
+  return (
+    <div>
+      {/* Add content for the JobDescription page */}
+      <h2>Universaltankandfabrication</h2>
+      <p>This is the job description page.</p>
+    </div>
+  );
+}
+
+// Define the HomeComponent we have home background information about me and contact me
 function HomeComponent() {
   return (
     <nav>
       <Link to="/">Home</Link>
-      <Link to="/innovative-brief">Innovative Brief</Link>
-      <Link to="/resources">Resources</Link>
-      <Link to="/about-me">About us</Link>
+      <Link to="/employment-information">Background Information</Link>
+      <Link to="/Skills">Skills</Link>
+      <Link to="/ContactMe">Contact Me</Link>
     </nav>
   );
 }
@@ -157,9 +233,14 @@ function App() {
       <HomeComponent />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/innovative-brief" element={<InnovativeBrief />} />
-        <Route path="/resources" element={<Resources />} />
-        <Route path="/about-me" element={<AboutUs />} />
+        <Route path="/employment-information" element={<EmploymentInformation />} />
+        <Route path="/Skills" element={<Skills />} />
+        <Route path="/ContactMe" element={<ContactMe />} />
+
+        <Route path="/PMIAerospace" element={<PMIAerospace />} />
+        <Route path="/MacLeanFoggCompany" element={<MacLeanFoggCompany />} />
+        <Route path="/SPGinternational" element={<SPGinternational />} />
+        <Route path="/Universaltankandfabrication" element={<Universaltankandfabrication />} />
       </Routes>
     </Router>
   );
