57767935d52cd63f189035b00cef38655341ba17
TrueErmak <ecanada07@gmail.com>
2024-01-30 20:11:55 -0600

    added job information and background information

--- src/App.js
+++ src/App.js
@@ -206,18 +206,7 @@ function Skills() {
           </iframe>
         </div>
         
-        {/* */}
-        <div className="video-container">
-          <iframe 
-            width="560" 
-            height="315" 
-            src="https://www.youtube.com/embed/FipxOVlKso4" 
-            title="YouTube video player" 
-            frameBorder="0" 
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-            allowFullScreen>
-          </iframe>
-        </div>
+        
        
 
         <p className="brief-header"> programing </p>
